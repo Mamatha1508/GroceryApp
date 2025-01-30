c0c8b7575e8e59cf0c857978aed2face849ac03a
Mamatha <aptyuser@Aptys-MacBook-Pro.local>
2025-01-30 11:58:47 +0530

    added shimmer ui and debouncing logic for search input

---
+++ src/components/Shimmer.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const Shimmer=()=>{
+    return (
+        <div className="shimmer-container">
+            <div className="shimmer-ui"></div>
+            <div className="shimmer-ui"></div>
+            <div className="shimmer-ui"></div>
+            <div className="shimmer-ui"></div>
+            <div className="shimmer-ui"></div>
+            <div className="shimmer-ui"></div>
+            <div className="shimmer-ui"></div>
+            <div className="shimmer-ui"></div>
+            <div className="shimmer-ui"></div>
+
+        </div>
+    )
+
+}
+
+export default Shimmer
